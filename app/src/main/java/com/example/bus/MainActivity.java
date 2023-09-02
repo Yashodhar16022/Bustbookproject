f46c04ac41895d7191a516598d9e1bcfca228bfa
yashodhar.p.d <yashd126@>
2023-09-02 16:13:16 +0530

    Final Commit

---
+++ app/src/main/java/com/example/bus/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.bus;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import  android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button search_buses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        search_buses = findViewById(R.id.search_buses);
+        search_buses.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+            startActivity(intent);
+        });
+    }
+}
